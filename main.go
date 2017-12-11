6c9337b2108da1916ceaada48ff27850a712f12e
topgiter <devstarstefan@gmail.com>
2017-12-11 08:01:17 +0000

    Added env varialbles

--- main.go
+++ main.go
@@ -1,34 +1,57 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+    "bytes"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
-	"github.com/russross/blackfriday"
+    "github.com/russross/blackfriday"
 )
 
+var (
+    repeat int
+)
+
+func repeatHandler(c *gin.Context) {
+    var buffer bytes.Buffer
+    for i := 0; i < repeat; i++ {
+        buffer.WriteString("Hello from Go!\n")
+    }
+    c.String(http.StatusOK, buffer.String())
+}
+
 func main() {
-	port := os.Getenv("PORT")
+    var err error
+    port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+    tStr := os.Getenv("REPEAT")
+    repeat, err = strconv.Atoi(tStr)
+    if err != nil {
+        log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
+        repeat = 5
+    }
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+    router := gin.New()
+    router.Use(gin.Logger())
+    router.LoadHTMLGlob("templates/*.tmpl.html")
+    router.Static("/static", "static")
 
-	router.GET("/mark", func(c *gin.Context) {
-		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
-	})
-	
-	router.Run(":" + port)
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl.html", nil)
+    })
+
+    router.GET("/mark", func(c *gin.Context) {
+        c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+    })
+
+    router.GET("/repeat", repeatHandler)
+
+    router.Run(":" + port)
 }
